a736412e82782fdcf5b91792127d8285ef654649
rafaelsa99 <rafa.sa_@hotmail.com>
2021-06-01 15:46:10 +0100

    Control Panel (missing JButton on JTable)

--- src/ControlPanel/package-info.java
+++ src/ControlPanel/package-info.java
@@ -1,11 +1,5 @@
 /**
 * This package contains the classes and methods to play the Control Panel role.
-* This package is not mandatory. If you want to launch your entities (clients,
-* servers, LB and Monitor) through a centralized entity developed by you,
-* than this is for you. If you want to launch your entities in a one e by one
-* basis through your IDE, than this is not for you. 
-* Remember, this is OPTIONAL!
-* 
 **/
 
-package Client;
+package ControlPanel;
